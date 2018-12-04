adaa98c1184defa30f0453a7f23122a9da5f8ee9
Asia Carter <asiamcarter@gmail.com>
2018-12-04 10:53:14 -0600

    debugged 'C is for Cookie', 'Conjunction Function', and 'Mod Squad'

---
+++ main.js
@@ -0,0 +1,40 @@
+
+// C is for Cookie
+const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+
+
+for (let x = 1; x < cookies.length; x++) {
+    const currentCookie = cookies[x]
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+}
+
+// Conjunction Function
+
+const conjunction = function (firstWord, secondWord) {
+    const conjoinedWord = `${firstWord} ${secondWord}`
+    console.log(conjoinedWord)
+}
+
+conjunction("Master", "Card")
+
+// Mod Squad
+
+{
+    const ModSquad = {
+        "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+        "series": {
+            "start": "1968",
+            "end": "1973"
+        }
+    }
+
+    let HTMLRepresentation = `<h1>The Mod Squad</h1>`
+
+    ModSquad.members.forEach(member => {
+        HTMLRepresentation += `<div>${member}</div>`
+    })
+    document.querySelector(".show-info").innerHTML = HTMLRepresentation
+}
+
+
+
